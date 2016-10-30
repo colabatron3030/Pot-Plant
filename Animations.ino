5e1e0f35cb946fdc7541806a912889097a10b4da
Unknown <isinfier@gmail.com>
2016-10-30 21:16:58 +1300

    First file upload

---
+++ Animations.ino
@@ -0,0 +1,146 @@
+#include <math.h>
+
+const int NUM_ANIMATIONS = 4;
+
+int   animation = 2;
+long  startTime = 0;
+float time      = 0;
+
+
+void loopAnimation()
+{
+  time = (millis() - startTime) / 1000.0;
+  
+  switch ( animation )
+  {
+    case 0 : animBlink(); break;
+    case 1 : animColourWheel(); break;
+    case 2 : animBreathe(); break;
+    case 3 : animKITT(); break;
+  }
+  
+  if ( time > 1 && digitalRead(PIN_BUTTON) == HIGH )
+  {
+    animation = (animation + 1) % NUM_ANIMATIONS;
+    startTime = millis();
+  }
+}
+
+
+void animBlink()
+{
+  byte r,g,b;
+  for ( int i = 0 ; i < NUM_LEDS ; i++ )
+  {
+    float frac = (float) i / NUM_LEDS;
+    float v    = fmod((time + frac) * 250, 250) > 125 ? 100 : 0; 
+    HSV_to_RGB(time * 5, 100, v, &r, &g, &b);
+    leds.setPixelColor(i, leds.Color(r, g, b));
+  }
+  leds.show();
+}
+
+
+void animColourWheel()
+{
+  byte r,g,b;
+  for ( int i = 0 ; i < NUM_LEDS ; i++ )
+  {
+    float frac = (float) i / NUM_LEDS;
+    float h = (time * 50) + (frac * 360);
+    HSV_to_RGB(h, 100, 100, &r, &g, &b);
+    leds.setPixelColor(i, leds.Color(r, g, b));
+  }
+  leds.show();
+}
+
+
+void animBreathe()
+{
+  byte r,g,b;
+  time = (millis() - startTime) / 1000.0;
+  
+  for ( int i = 0 ; i < NUM_LEDS ; i++ )
+  {
+    float frac = (float) i / NUM_LEDS;
+    byte  s    = 50 + 50 * sin(time * 4 + frac * 2 * PI);
+    byte  v    = 0 + 50 * -1 *sin(time * 4 + frac * 2 * PI);
+    HSV_to_RGB(120, s, v, &r, &g, &b);
+    leds.setPixelColor(i, leds.Color(r, g, b));
+  }
+  leds.show();
+}
+
+
+void animKITT()
+{
+  byte r,g,b;
+  leds.clear();
+  HSV_to_RGB(time * 5, 100, 100, &r, &g, &b);
+  r = 255; g = 0; b =0 ;
+  leds.setPixelColor((int) ((NUM_LEDS - 0.01) * (0.5 + 0.5 * sin(time * 5))), leds.Color(r, g, b));
+  HSV_to_RGB(180 + time * 5, 100, 100, &r, &g, &b);
+  r = 0 ; g = 255; b= 0;
+  leds.setPixelColor((int) ((NUM_LEDS - 0.01) * (0.5 + 0.5 * cos(time * 5))), leds.Color(r, g, b));
+  leds.show();
+}
+
+
+
+//
+// Convert HSV values to RGB
+// Source: https://gist.github.com/hdznrrd/656996
+//
+void HSV_to_RGB(float h, float s, float v, byte *r, byte *g, byte *b)
+{
+  h = fmod(h, 360.0);
+  s = max(0.0, min(100.0, s)) / 100;
+  
+  if(s == 0) {
+    // Achromatic (grey)
+    *r = *g = *b = round(v*255);
+    return;
+  }
+
+  v = max(0.0, min(100.0, v)) / 100;
+
+  h /= 60; // sector 0 to 5
+  int   i = floor(h);
+  float f = h - i; // factorial part of h
+  
+  float p = v * (1 - s);
+  float q = v * (1 - s * f);
+  float t = v * (1 - s * (1 - f));
+  switch(i) {
+    case 0:
+      *r = round(255*v);
+      *g = round(255*t);
+      *b = round(255*p);
+      break;
+    case 1:
+      *r = round(255*q);
+      *g = round(255*v);
+      *b = round(255*p);
+      break;
+    case 2:
+      *r = round(255*p);
+      *g = round(255*v);
+      *b = round(255*t);
+      break;
+    case 3:
+      *r = round(255*p);
+      *g = round(255*q);
+      *b = round(255*v);
+      break;
+    case 4:
+      *r = round(255*t);
+      *g = round(255*p);
+      *b = round(255*v);
+      break;
+    default: // case 5:
+      *r = round(255*v);
+      *g = round(255*p);
+      *b = round(255*q);
+    }
+}
+
---
+++ PotPlant_1.79__works_.ino
@@ -0,0 +1,138 @@
+#include <Adafruit_NeoPixel.h>
+
+/*
+  # Moisture sensor code
+  #
+  # Connect the sensor to an A pin(Analog 0/1 on the Arduino board
+
+  # the sensor value description
+  # 0  ~300     dry soil
+  # 300~700     humid soil
+  # 700~950     in water
+*/
+
+int moisturePin = A0;
+int soilMoisture = 0;
+
+int UVPin = A2; //Output from the UV sensor
+
+//float uvLowThreshold = ;
+//float uvHighThreshold = ;
+
+int mThresholdHigh = 700;
+int mThresholdLow  = 250;
+
+const int PIN_DATA   = 6;  // data pin
+const int PIN_BUTTON = 3;  // button pin
+const int NUM_LEDS   = 35; // number of LEDs
+
+// Parameter 1 = number of pixels in strip,  neopixel stick has 8
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_RGB     Pixels are wired for RGB bitstream
+//   NEO_GRB     Pixels are wired for GRB bitstream, correct for neopixel stick
+//   NEO_KHZ400  400 KHz bitstream (e.g. FLORA pixels)
+//   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip), correct for neopixel stick
+
+Adafruit_NeoPixel leds = Adafruit_NeoPixel(NUM_LEDS, PIN_DATA, NEO_GRB + NEO_KHZ800);
+//bool oldState = HIGH;  from buttoncycle example sketch
+//int showType = 0;
+
+
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(moisturePin, INPUT);
+  pinMode(UVPin, INPUT);
+  pinMode(PIN_BUTTON, INPUT);
+  leds.begin();           // initialise the NeoPixel library
+  leds.setBrightness(40); // set global brightness fairly low
+  leds.show();
+
+
+}
+
+
+void loop() {
+
+  int uvLevel = averageAnalogRead(UVPin);
+
+  float outputVoltage = 5.0 * uvLevel / 1024;
+  float uvIntensity = mapfloat(outputVoltage, 0.99, 2.9, 0.0, 15.0);
+
+  //set analog read from A0 pin to soilMoisture variable
+  soilMoisture = analogRead(moisturePin);
+  Serial.print("Moisture Sensor Value:");
+  Serial.println(soilMoisture);
+
+//  Serial.print("UVAnalogOutput: ");
+//  Serial.print(uvLevel);
+//
+//  Serial.print(" OutputVoltage: ");
+//  Serial.print(outputVoltage);
+//
+//  Serial.print(" UV Intensity: ");
+//  Serial.print(uvIntensity);
+//  Serial.print(" mW/cm^2 ");
+//  Serial.print("Red: ");
+//  Serial.println();
+
+  delay(100);
+  
+  
+
+
+
+  //loopBlink();
+  //loopAnimation();
+
+
+  // If soil moisture is less than the lower threshold activate the breathe animation
+  // ,if above the high threshold active the blink animation
+  if (soilMoisture < mThresholdLow) {
+    animBreathe();
+  }
+  else if (soilMoisture > mThresholdHigh) {
+    animColourWheel();
+  }
+
+}
+
+
+// takes the average reading on the given pin and returns that average.
+int averageAnalogRead(int pinToRead)
+{
+  byte numberOfReadings = 8;
+  unsigned int runningValue = 0;
+
+  for (int x = 0 ; x < numberOfReadings ; x++)
+    runningValue += analogRead(pinToRead);
+  runningValue /= numberOfReadings;
+
+  return (runningValue);
+
+}
+
+// Map function in Arudino but for floats
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void loopBlink()
+{
+  // prepare colours (does not actually change the LED colours yet)
+  leds.setPixelColor(0, leds.Color(255, 0, 0)); // first  LED red
+  leds.setPixelColor(1, leds.Color(0, 255, 0)); // second LED green
+  leds.setPixelColor(2, leds.Color(0, 0, 255)); // third  LED blue
+  leds.show(); // send the updated pixel colours to the LEDs
+  delay(1000);
+
+  // and a bit darker
+  leds.setPixelColor(0, leds.Color(100, 0, 0)); // first  LED red
+  leds.setPixelColor(1, leds.Color(0, 100, 0)); // second LED green
+  leds.setPixelColor(2, leds.Color(0, 0, 100)); // third  LED blue
+  leds.show(); // send the updated pixel colours to the LEDs
+  delay(1000);
+
+}
